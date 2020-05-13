91324083b3743fdee7e9b8b06805fd5997c82082
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-05-13 11:06:42 +0200

    fix indentation

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+
 const app = express();
 const port = 3000;
 
 app.use(express.static('public/'));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 app.get('/', (req, res) => res.redirect(`http://localhost:${port}/add_account.html`));
+
+
 
 // app.listen(port, function () {
 //     console.log('Express-server started on http://localhost:3000/add_account.html');
